4599c1cb2c15e185ff38af370e22c4f019eb1b68
Josh Moulder <56400569+OiMoulder@users.noreply.github.com>
2024-04-15 01:22:18 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Quiz from './Quiz';
 import HoldingPage from './Holding';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={HoldingPage} />
+        <Route path="/holding" exact element={HoldingPage} />
         <Route path="/quiz" element={Quiz} />
       </Routes>
     </Router>
